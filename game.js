e8abe1027c4887559b42596c1bd0f026c1131fd6
Amolkamat <amolkamatmhamai@gmail.com>
2017-04-13 10:35:12 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-ar computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //Setting all to zero
 var wins = 0;
